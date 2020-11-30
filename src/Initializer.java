64426f33d4c5e48362e0badc2c7b2447903f055b
danila <manturov.dan@mail.ru>
2020-11-30 17:06:39 +0300

    starting to redo proj

--- src/Initializer.java
+++ src/Initializer.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class Initializer {
     public static void main(String[] args) throws Exception {
         System.out.println("Start the project");
-        MainController controller = new MainController(0, new File("resources/calibration.yaml"));
+        MainController controller = new MainController(0, new File("resources/calibration_old.yaml"));
         controller.run();
     }
 }
