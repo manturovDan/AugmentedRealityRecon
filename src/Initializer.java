21cb11f2a8662b514de633ca3dc502a041a3d70d
danila <manturov.dan@mail.ru>
2020-12-02 22:02:25 +0300

    clena more

--- src/Initializer.java
+++ src/Initializer.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class Initializer {
     public static void main(String[] args) throws Exception {
         System.out.println("Start the project");
-        MainController controller = new MainController(0, new File("resources/calibration.yaml"));
+        MainController controller = new MainController(Integer.parseInt(args[0]), new File(args[1]));
         controller.run();
     }
 }
